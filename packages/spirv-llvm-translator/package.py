8668794d25a6bed03f230e19fb8f1b3e2e43e250
Philip C. Roth <rothpc@ornl.gov>
2022-12-01 15:36:49 -0500

    Add SPIRV-related packages.

---
+++ packages/spirv-llvm-translator/package.py
@@ -0,0 +1,29 @@
+import sys
+from spack import *
+
+class SpirvLlvmTranslator(CMakePackage):
+
+    homepage = 'https://github.com/KhronosGroup/SPIRV-LLVM-Translator'
+    git = 'https://github.com/KhronosGroup/SPIRV-LLVM-Translator'
+    url = 'https://github.com/KhronosGroup/SPIRV-LLVM-Translator/archive/refs/tags/v15.0.0.tar.gz'
+
+    # Maintainer of the Spack package, not the software itself.
+    maintainers = ['rothpc@ornl.gov']
+
+    version('15.0.0', tag='v15.0.0')
+
+    variant('tools', default=True, description='Integrate SPIRV-Tools')
+
+    depends_on('llvm@15', when='@15.0.0')
+    depends_on('spirv-tools', when='+tools')
+
+    resource(
+            name='spirv-headers',
+            git='https://github.com/KhronosGroup/SPIRV-Headers',
+            destination='external'
+    )
+
+    def setup_build_environment(self, env):
+        if '+tools' in self.spec:
+            env.append_path("PKG_CONFIG_PATH", self.spec['spirv-tools'].prefix.lib.pkgconfig)
+
---
+++ packages/spirv-tools/package.py
@@ -0,0 +1,40 @@
+import sys
+from spack import *
+
+class SpirvTools(CMakePackage):
+
+    homepage = 'https://github.com/KhronosGroup/SPIRV-Tools'
+    git = 'https://github.com/KhronosGroup/SPIRV-Tools'
+    url = 'https://github.com/KhronosGroup/SPIRV-Tools/archive/refs/tags/v2022.4.tar.gz'
+
+    # Maintainer of the Spack package, not the software itself.
+    maintainers = ['rothpc@ornl.gov']
+
+    version('2022.4', tag='v2022.4')
+    version('2022.3', tag='v2022.3')
+    version('2022.2', tag='v2022.2')
+    version('2022.1', tag='v2022.1')
+
+    depends_on('python@3')
+
+    resource(
+            name='spirv-headers',
+            git='https://github.com/KhronosGroup/SPIRV-Headers',
+            destination='external'
+    )
+    resource(
+            name='re2',
+            git='https://github.com/google/re2',
+            destination='external'
+    )
+    resource(
+            name='effcee',
+            git='https://github.com/google/effcee',
+            destination='external'
+    )
+    resource(
+            name='googletest',
+            git='https://github.com/google/googletest',
+            destination='external'
+    )
+
