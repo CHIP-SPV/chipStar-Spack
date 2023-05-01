375e1224f97d238ecce1d99761d3d849d18d24c9
Philip C. Roth <rothpc@ornl.gov>
2023-05-01 14:14:00 +0000

    Add Spack support for recent LLVM, SPIRV package versions.

--- Repo/packages/spirv-llvm-translator/package.py
+++ Repo/packages/spirv-llvm-translator/package.py
@@ -7,16 +7,18 @@ class SpirvLlvmTranslator(CMakePackage):
 
     homepage = 'https://github.com/KhronosGroup/SPIRV-LLVM-Translator'
     git = 'https://github.com/KhronosGroup/SPIRV-LLVM-Translator'
-    url = 'https://github.com/KhronosGroup/SPIRV-LLVM-Translator/archive/refs/tags/v15.0.0.tar.gz'
+    url = 'https://github.com/KhronosGroup/SPIRV-LLVM-Translator/archive/refs/tags/v16.0.0.tar.gz'
 
     # Maintainer of the Spack package, not the software itself.
-    maintainers = ['rothpc@ornl.gov']
+    maintainers = ['rothpc']
 
+    version('16.0.0', tag='v16.0.0')
     version('15.0.0', tag='v15.0.0')
     version('14.0.0', tag='v14.0.0')
 
     variant('tools', default=True, description='Integrate SPIRV-Tools')
 
+    depends_on('llvm@16', when='@16.0.0')
     depends_on('llvm@15', when='@15.0.0')
     depends_on('llvm@14', when='@14.0.0')
     depends_on('spirv-tools', when='+tools')
--- Repo/packages/spirv-tools/package.py
+++ Repo/packages/spirv-tools/package.py
@@ -10,14 +10,25 @@ class SpirvTools(CMakePackage):
     url = 'https://github.com/KhronosGroup/SPIRV-Tools/archive/refs/tags/v2022.4.tar.gz'
 
     # Maintainer of the Spack package, not the software itself.
-    maintainers = ['rothpc@ornl.gov']
+    maintainers = ['rothpc']
 
+    version('2023.2', tag='v2023.2')
+    version('2023.1', tag='v2023.1')
     version('2022.4', tag='v2022.4')
     version('2022.3', tag='v2022.3')
     version('2022.2', tag='v2022.2')
     version('2022.1', tag='v2022.1')
 
+    variant('cxxstd',
+            description='Use the given C++ standard when compiling',
+            values=('11', '14', '17'),
+            default='17',
+            multi=False)
+
     depends_on('python@3')
+
+    conflicts('cxxstd=11', when='@:2023.1')
+    conflicts('cxxstd=14', when='@:2023.2')
 
     resource(
             name='spirv-headers',
@@ -39,4 +50,13 @@ class SpirvTools(CMakePackage):
             git='https://github.com/google/googletest',
             destination='external'
     )
+
+    def cmake_args(self):
+
+        args = [
+            self.define_from_variant('CMAKE_CXX_STANDARD', 'cxxstd'),
+            '-DCMAKE_CXX_EXTENSIONS=OFF',
+        ]
+
+        return args
 
