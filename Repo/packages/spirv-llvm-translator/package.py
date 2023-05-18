8fd3c7e7a30e8618095e6535de20861b6968d456
Philip C. Roth <rothpc@ornl.gov>
2023-05-18 13:20:38 +0000

    Add clang@16 support for spirv-llvm-translator too.

--- Repo/packages/spirv-llvm-translator/package.py
+++ Repo/packages/spirv-llvm-translator/package.py
@@ -7,18 +7,18 @@ class SpirvLlvmTranslator(CMakePackage):
 
     homepage = 'https://github.com/KhronosGroup/SPIRV-LLVM-Translator'
     git = 'https://github.com/KhronosGroup/SPIRV-LLVM-Translator'
-    url = 'https://github.com/KhronosGroup/SPIRV-LLVM-Translator/archive/refs/tags/v15.0.0.tar.gz'
+    url = 'https://github.com/KhronosGroup/SPIRV-LLVM-Translator/archive/refs/tags/v16.0.0.tar.gz'
 
     # Maintainer of the Spack package, not the software itself.
-    maintainers = ['rothpc@ornl.gov']
+    maintainers = ['rothpc']
 
-    version('15.0.0', tag='v15.0.0')
-    version('14.0.0', tag='v14.0.0')
+
+    supported_major_versions = [14, 15, 16]
+    for curr_version in supported_major_versions:
+        version(f'{curr_version}.0.0', tag=f'v{curr_version}.0.0')
+        depends_on(f'llvm@{curr_version}', when=f'@{curr_version}.0.0')
 
     variant('tools', default=True, description='Integrate SPIRV-Tools')
-
-    depends_on('llvm@15', when='@15.0.0')
-    depends_on('llvm@14', when='@14.0.0')
     depends_on('spirv-tools', when='+tools')
 
     def setup_build_environment(self, env):
